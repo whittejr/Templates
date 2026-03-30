37fc943fc3f6078f9bfc1a3a6dd052bdbc50688c
whittejr <ale.rebelo48@gmail.com>
2026-03-30 03:46:44 +0000

    300326

--- cmake/mcu/stm32wb.cmake
+++ cmake/mcu/stm32wb.cmake
@@ -1,42 +1,33 @@
-# ==============================================================================
-# IDENTIDADE DO HARDWARE: STM32WB Series
-# ==============================================================================
 message(STATUS "Configurando paths e flags para a familia STM32WB...")
 
 # Caminho base do submódulo que baixamos no terminal
-set(VENDOR_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/stm32cubewb/Drivers")
+set(THIRD_PARTY "${CMAKE_CURRENT_SOURCE_DIR}/third_party/stm32cubewb/Drivers")
 
-# 1. DEFINIÇÃO DO STARTUP (Assembly) E LINKER SCRIPT
-# Geralmente o Linker você copia pra sua pasta, ou usa o da ST
-set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/Core/linker/stm32wb55xx_flash_cm4.ld")
-set(STARTUP_FILE "${VENDOR_DIR}/CMSIS/Device/ST/STM32WBxx/Source/Templates/gcc/startup_stm32wb55xx_cm4.s")
+set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/core/linker/stm32wb55xx_flash_cm4.ld")
+set(STARTUP_FILE "${CMAKE_CURRENT_SOURCE_DIR}/core/startup_stm32wb55xx_cm4.s")
 
-# ==============================================================================
-# 2. FONTES DA HAL (.c)
-# ==============================================================================
-# Aqui você lista os periféricos da ST que o seu projeto vai usar
 set(HAL_SOURCES
     ${STARTUP_FILE}
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_cortex.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc_ex.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_gpio.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr.c"
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr_ex.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_cortex.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_rcc_ex.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_gpio.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_dma.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr.c"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Src/stm32wbxx_hal_pwr_ex.c"
     
     # Arquivo de inicialização do relógio do sistema (System Clock)
-    "${VENDOR_DIR}/CMSIS/Device/ST/STM32WBxx/Source/Templates/system_stm32wbxx.c"
+    "${THIRD_PARTY}/CMSIS/Device/ST/STM32WBxx/Source/Templates/system_stm32wbxx.c"
 )
 
 # ==============================================================================
 # 3. INCLUDES DA HAL (.h)
 # ==============================================================================
 set(HAL_INCLUDES
-    "${VENDOR_DIR}/STM32WBxx_HAL_Driver/Inc"
-    "${VENDOR_DIR}/CMSIS/Device/ST/STM32WBxx/Include"
-    "${VENDOR_DIR}/CMSIS/Core/Include"
+    "${THIRD_PARTY}/STM32WBxx_HAL_Driver/Inc"
+    "${THIRD_PARTY}/CMSIS/Device/ST/STM32WBxx/Include"
+    "${THIRD_PARTY}/CMSIS/core/Include"
 )
 
 # Definições globais para o código C saber qual é a placa
